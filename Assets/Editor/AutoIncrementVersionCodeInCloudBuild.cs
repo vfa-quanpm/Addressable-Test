76f38bf4819b213b1b429840de18f5147e087382
ThienVFA <thiennv@vitalify.asia>
2023-11-27 09:41:50 +0700

    Fix bug

--- Assets/Editor/AutoIncrementVersionCodeInCloudBuild.cs
+++ Assets/Editor/AutoIncrementVersionCodeInCloudBuild.cs
@@ -1,6 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using System;
 
 public class AutoIncrementVersionCodeInCloudBuild : MonoBehaviour
 {
