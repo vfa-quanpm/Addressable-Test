c0c9cc977becfebcc438a5aa3beb6881b7c1f6d6
ThienVFA <thiennv@vitalify.asia>
2023-11-27 09:15:14 +0700

    Test Auto increment VersionCode in cloud build

---
+++ Assets/Editor/AutoIncrementVersionCodeInCloudBuild.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoIncrementVersionCodeInCloudBuild : MonoBehaviour
+{
+#if UNITY_CLOUD_BUILD
+    public static void PreExport(UnityEngine.CloudBuild.BuildManifestObject manifest)
+    {
+        string buildNumber = manifest.GetValue("buildNumber", "0");
+        Debug.LogWarning("Setting build number to " + buildNumber);
+        PlayerSettings.Android.bundleVersionCode = int.Parse(buildNumber);
+        PlayerSettings.iOS.buildNumber = buildNumber;
+    }
+#endif
+}
