325172ea5d7231efb63e152fa78cd4ddb3861759
ThienVFA <thiennv@vitalify.asia>
2023-12-05 14:11:33 +0700

    fix bug

--- Assets/Editor/AutoIncrementVersionCodeInCloudBuild.cs
+++ Assets/Editor/AutoIncrementVersionCodeInCloudBuild.cs
@@ -9,7 +9,7 @@ public class AutoIncrementVersionCodeInCloudBuild : MonoBehaviour
     {
         string buildNumber = manifest.GetValue("buildNumber", "0");
         int buildNumberInt = int.Parse(buildNumber);
-        int bundleVersion = int.Parse(PlayerSettings.bundleVersion);
+        int bundleVersion = int.Parse(PlayerSettings.Android.bundleVersion);
         if(buildNumberInt < bundleVersion)
         {
             buildNumberInt = bundleVersion + 1;
